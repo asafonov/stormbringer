61e18b6dbe83170664296dc91ff049d8efd88fbb
Alex Safonov <me@asafonov.org>
2013-05-13 08:45:50 +0400

    GTK3 gui first steps. Added message list and textbox. Just studying for now

--- stormbringer_gtk.py
+++ stormbringer_gtk.py
@@ -1,6 +1,64 @@
 from gi.repository import Gtk
+import os, sys, re, threading, shutil
+import lib.asafonov_pop3, lib.asafonov_imap, lib.asafonov_smtp, lib.asafonov_folders
 
-win = Gtk.Window()
+class emailGui(Gtk.Window):
+    def __init__(self, program_folder=''):
+        Gtk.Window.__init__(self, title="Stormbringer")
+        self.program_folder = program_folder
+        self.createWidgets()
+        self.printArchiveMessageList()
+
+    def createWidgets(self):
+        self.grid = Gtk.Grid()
+        self.add(self.grid)
+
+        self.liststore = Gtk.ListStore(str, str, str, str)
+        treeview = Gtk.TreeView(model=self.liststore)
+        renderer_text = Gtk.CellRendererText()
+        column_text = Gtk.TreeViewColumn("Num", renderer_text, text=0)
+        treeview.append_column(column_text)
+        renderer_text = Gtk.CellRendererText()
+        column_text = Gtk.TreeViewColumn("Subject", renderer_text, text=1)
+        treeview.append_column(column_text)
+        renderer_text = Gtk.CellRendererText()
+        column_text = Gtk.TreeViewColumn("From", renderer_text, text=2)
+        treeview.append_column(column_text)
+        renderer_text = Gtk.CellRendererText()
+        column_text = Gtk.TreeViewColumn("Date", renderer_text, text=3)
+        treeview.append_column(column_text)
+        self.grid.attach(treeview,0,0,3,1)
+
+        scrolledwindow = Gtk.ScrolledWindow()
+        scrolledwindow.set_hexpand(True)
+        scrolledwindow.set_vexpand(True)
+        self.grid.attach(scrolledwindow,0,1,3,1)
+        self.textview = Gtk.TextView()
+        self.textbuffer = self.textview.get_buffer()
+        self.textbuffer.set_text("")
+        scrolledwindow.add(self.textview)
+
+    def printList(self):
+        self.liststore.clear()
+        for i in range(len(self.message_list)):
+            tmp = [str(i+1), self.message_list[i]['Subject'], self.message_list[i]['From'], self.message_list[i]['Date']]
+            self.liststore.append(tmp)
+
+    #
+    # -- printArchiveMessageList
+    # prints archive messages
+    #
+    def printArchiveMessageList(self):
+        self.message_list = lib.asafonov_folders.getArchiveFolderItems(self.program_folder)
+        self.printList()
+
+
+if len(sys.argv)>1:
+    program_folder = sys.argv[1]
+else:
+    program_folder = ''
+
+win = emailGui(program_folder=program_folder)
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
 Gtk.main()
